b21ae4d7847cc89520ed3d878aac553c308d5525
jiangce <jiangce@togeek.cn>
2016-01-21 15:48:40 +0800

    修改默认参数

--- file_express/__init__.py
+++ file_express/__init__.py
@@ -12,7 +12,7 @@ env = Environment()
 
 
 def init_env():
-    work_path = get_input('工作路径：', default=r'c:\togeek')
+    work_path = get_input('工作路径：', default=r'c:\togeek\fexp')
     error_count = get_input('错误重试次数：', int, 5)
     env.write_config(work_path, error_count)
     print(env)
